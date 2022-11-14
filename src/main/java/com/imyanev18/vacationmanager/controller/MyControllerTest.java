6410db79c9ad00c4a8f4a9a5de590bc6fe6bcba2
IMYanev18 <IMYanev18@codingburgas.bg>
2022-11-14 13:57:22 +0200

    adding stage 1 files

---
+++ src/main/java/com/imyanev18/vacationmanager/controller/MyControllerTest.java
@@ -0,0 +1,9 @@
+package com.imyanev18.vacationmanager.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class MyControllerTest {
+
+
+
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO Users (ID, Username, Password, PersonalName, FamilyName, Roles, Team )
+VALUES (1, 'iyanev66','taina','Ilian','Yanev','god', 'ZaFe');
